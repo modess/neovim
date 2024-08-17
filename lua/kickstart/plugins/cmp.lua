f5e309146906e7e7d5b8219bb8cdf5b97a229d36
Niklas Modess <niklas@modess.io>
2024-08-17 10:16:04 +0200

    lspkind

--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -3,6 +3,7 @@ return {
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
     dependencies = {
+      { 'onsails/lspkind.nvim' },
       {
         'zbirenbaum/copilot-cmp',
         dependencies = {
@@ -60,7 +61,47 @@ return {
       local luasnip = require 'luasnip'
       luasnip.config.setup {}
 
+      local lspkind = require 'lspkind'
+
       cmp.setup {
+        formatting = {
+          format = lspkind.cmp_format {
+            mode = 'symbol', -- show only symbol annotations
+            maxwidth = 100, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            -- can also be a function to dynamically calculate max width such as
+            -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+            show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+            symbol_map = {
+              Copilot = '',
+              Text = '󰉿',
+              Method = '󰆧',
+              Function = '󰊕',
+              Constructor = '',
+              Field = '󰜢',
+              Variable = '󰀫',
+              Class = '󰠱',
+              Interface = '',
+              Module = '',
+              Property = '󰜢',
+              Unit = '󰑭',
+              Value = '󰎠',
+              Enum = '',
+              Keyword = '󰌋',
+              Snippet = '',
+              Color = '󰏘',
+              File = '󰈙',
+              Reference = '󰈇',
+              Folder = '󰉋',
+              EnumMember = '',
+              Constant = '󰏿',
+              Struct = '󰙅',
+              Event = '',
+              Operator = '󰆕',
+              TypeParameter = '',
+            },
+          },
+        },
         snippet = {
           expand = function(args)
             luasnip.lsp_expand(args.body)
